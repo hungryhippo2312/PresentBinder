78863bb07ab49f28277e4469141cd1b897558815
Stuart Leyland <stuartleyland@gmail.com>
2012-07-29 10:33:53 +0100

    Added present view model
    Added a view model to display a "present" entity.  Currently this just
    shows the description of the present.

---
+++ DontForgetThePresents/DontForgetThePresents/ViewModel/PresentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using DontForgetThePresents.Models;
+
+namespace DontForgetThePresents.ViewModel
+{
+    public class PresentViewModel : ViewModelBase
+    {
+        private Present _present;
+
+        public PresentViewModel(Present present)
+        {
+            _present = present;
+        }
+
+        public String PresentName
+        {
+            get
+            {
+                return _present.Description;
+            }
+            set
+            {
+                if (value != _present.Description)
+                {
+                    _present.Description = value;
+                    RaisePropertyChanged("PresentName");
+                }
+            }
+        }
+    }
+}
